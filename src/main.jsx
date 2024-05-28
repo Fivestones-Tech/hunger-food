22a292285aaf26fd7980d8d8bf14a53bc954711f
Ogunaike Matthew Folawe <fivestonestech@gmail.com>
2024-05-28 12:30:27 +0100

    Switch to HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import StoreContextprovider from "./context/StoreContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <Router>
     <StoreContextprovider>
       <App />
     </StoreContextprovider>
-  </BrowserRouter>
+  </Router>
 );
